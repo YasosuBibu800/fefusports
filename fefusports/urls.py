c2e45f0de6cc8ed81c1161c521c0ac74be31fd23
zenci125 <121859719+zenci125@users.noreply.github.com>
2024-01-30 13:55:19 +1000

    Add models and forms for app

--- fefusports/urls.py
+++ fefusports/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for fefusports project.
+"""coolsite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,11 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.urls import path
+
+from fefusports import settings
+from users.views import *
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include("users.urls")),
-    path('accounts/', include("django.contrib.auth.urls")),
+    path('', include('users.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = pageNotFound
